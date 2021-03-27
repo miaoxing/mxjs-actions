e17ef03208933e043df24bd9f9eb24412e511f16
twinh <twinhuang@qq.com>
2021-03-27 16:00:42 +0800

    fix: 忽略空节点

--- Actions.js
+++ Actions.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Actions = ({children}) => {
   const actions = [];
   React.Children.map(children, (child) => {
+    if (!child) {
+      return;
+    }
     actions.push(child, ' ');
   });
   return actions;
--- LinkActions.js
+++ LinkActions.js
@@ -4,6 +4,9 @@ import {Divider} from 'antd';
 const LinkActions = ({children}) => {
   const actions = [];
   React.Children.map(children, (child, i) => {
+    if (!child) {
+      return;
+    }
     actions.push(child, <Divider key={i} type="vertical"/>);
   });
   actions.pop();
