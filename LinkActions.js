b6b4a64d0bf4bda08abab0c3fd2c7c2c6a1df66c
twinh <twinhuang@qq.com>
2023-02-27 23:28:52 +0800

    feat(actions): `LinkActions` 改为继承 `Actions`

--- LinkActions.js
+++ LinkActions.js
@@ -1,16 +1,10 @@
-import { Children } from 'react';
 import {Divider} from 'antd';
+import Actions from './Actions';
 
-const LinkActions = ({children}) => {
-  const actions = [];
-  Children.map(children, (child, i) => {
-    if (!child) {
-      return;
-    }
-    actions.push(child, <Divider key={i} type="vertical"/>);
-  });
-  actions.pop();
-  return actions;
+const LinkActions = (props) => {
+  return (
+    <Actions divider={<Divider type="vertical"/>} {...props}/>
+  );
 };
 
 export default LinkActions;
